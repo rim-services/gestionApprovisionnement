e73c3fcf7ef7ae85328b92033998b55d095a960b
zahraabdelbaghi <95882366+zahraabdelbaghi@users.noreply.github.com>
2022-07-04 13:56:54 +0200

    edede

---
+++ front-end/front-end/pages/js2/Sortiedetail/update.js
@@ -0,0 +1,55 @@
+
+let temp;
+let s=0;
+let mod='create';
+var idEdit =parseInt(localStorage.getItem('myValue'));
+let url='http://localhost:8088/services/RS/admin';
+
+
+const addAdminForm = document.querySelector(".forms-sample");
+let nom=document.getElementById("nom");
+let prenom=document.getElementById("prenom");
+let email= document.getElementById("email");
+let pass=document.getElementById("password");
+updateData(idEdit)
+function update(){
+   
+  addAdminForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+  fetch(`${url}/${idEdit}`,{
+    method:'PUT',
+    headers:{
+      'Accept':'application/json',
+      'Content-Type':'application/json; charset=UTF-8',
+      // "Access-Control-Allow-Origin": "*",
+  },
+    body: JSON.stringify({
+  
+      nom: nom.value,
+      prenom: prenom.value,
+      pass: pass.value,
+      email: email.value
+      
+    }),
+  }) 
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(console.error);
+ 
+})
+//window.location.href="admin.html";
+    }
+
+function updateData(idEdit){
+  fetch(`${url}/${idEdit}`,{
+    method:'GEt',
+  }) 
+  .then(res => res.json())
+  .then(data=>{
+    nom.value=data.nom,
+    prenom.value=data.prenom,
+    email.value=data.email
+  })
+
+//update()
+}
